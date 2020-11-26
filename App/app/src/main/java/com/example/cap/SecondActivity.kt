1385b460319ca0bbfa2bd0f20db42f40cc2b3da4
qwa310 <wjdalsdkcla@naver.com>
2020-11-26 17:17:06 +0900

    add app

---
+++ App/app/src/main/java/com/example/cap/SecondActivity.kt
@@ -0,0 +1,10 @@
+package com.example.cap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+    }
+}
