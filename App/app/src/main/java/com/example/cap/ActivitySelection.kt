bb79f887cce3a3667e0260c2cdc6348bfe58653e
Hyesung <comet_0419@naver.com>
2020-12-01 18:48:38 +0900

    Add video

--- App/app/src/main/java/com/example/cap/ActivitySelection.kt
+++ App/app/src/main/java/com/example/cap/ActivitySelection.kt
@@ -1,14 +1,40 @@
 package com.example.cap
 
 import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
 
 class ActivitySelection : AppCompatActivity() {
+    lateinit var videoView: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection)
+
+        videoView = findViewById(R.id.videoView)
+        val videoUri = Uri.parse("android.resource://$packageName/${R.raw.lat_pulldown}")
+        videoView.setMediaController(MediaController(this))
+        videoView.setVideoURI(videoUri)
+
+        videoView.requestFocus()
+        videoView.start()
+
+        videoView.setOnCompletionListener {
+            Toast.makeText(applicationContext, "Video completed", Toast.LENGTH_LONG).show()
+        }
+        videoView.setOnErrorListener { mp, what, extra ->
+            Toast.makeText(applicationContext, "An error occured while playing video",
+                    Toast.LENGTH_LONG).show()
+            false
+        }
+
+
         val Set : Button = findViewById(R.id.Set)
         Set.setOnClickListener {
             val nextIntent = Intent(this, Exercise::class.java)
@@ -25,5 +51,15 @@ class ActivitySelection : AppCompatActivity() {
             val nextIntent = Intent(this, ExerciseWeightInput::class.java)
             startActivity(nextIntent)
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (videoView != null && videoView.isPlaying) videoView.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (videoView != null) videoView.stopPlayback()
     }
 }
