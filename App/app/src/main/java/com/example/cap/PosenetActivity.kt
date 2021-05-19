31077aa3e140c51f49164e6510b495e1d5fd3841
Hyesung <comet_0419@naver.com>
2021-05-20 02:25:57 +0900

    Modify layout & merge main branch

--- App/app/src/main/java/com/example/cap/PosenetActivity.kt
+++ App/app/src/main/java/com/example/cap/PosenetActivity.kt
@@ -261,6 +261,7 @@ class PosenetActivity :
         // We don't use a front facing camera in this sample.
         val cameraDirection = characteristics.get(CameraCharacteristics.LENS_FACING)
         if (cameraDirection != null && cameraDirection == CameraCharacteristics.LENS_FACING_BACK
+//        if (cameraDirection != null && cameraDirection == CameraCharacteristics.LENS_FACING_FRONT
         ) {
           continue
         }
@@ -416,6 +417,7 @@ class PosenetActivity :
       // Create rotated version for portrait display
       val rotateMatrix = Matrix()
       rotateMatrix.postRotate(270.0f)
+//      rotateMatrix.postRotate(90.0f)
 
       val rotatedBitmap = Bitmap.createBitmap(
         imageBitmap, 0, 0, previewWidth, previewHeight,
